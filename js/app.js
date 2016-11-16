5645d4412a63b238928a91a199b2ee624c9de772
zhukovsd <zhukovsd@gmail.com>
2016-11-16 20:20:16 +0300

    Less style for tiles, initial request to last.fm

--- js/app.js
+++ js/app.js
@@ -28,9 +28,11 @@ var gridRowByIndex = function(index) {
 
 var gridAdjustWholeRow = function(wholeRow) {
     var index = (gridRowByIndex(wholeRow.data("after-num")) + 1) * gridColCountByViewportSize() - 1;
-    console.log("index = " + index);
+    // console.log("insert after " + index);
 
-    var elem = $("[num='" + index + "']");
+    // var elem = $("[num='" + index + "']");
+    // var elem = $(".artist-grid-tile:nth-of-type(" + index + ")");
+    var elem = $(".artist-grid-tile").eq(index);
     wholeRow.insertAfter(elem);
 };
 
@@ -40,4 +42,18 @@ $(document).ready(function() {
             // alert("bottom!");
         }
     });
+
+    $.get("http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=rock&api_key=1ad6bea80327069ed4ecccf76fe34175&limit=36&format=json")
+        .done(function(data) {
+            $.each(data.topartists.artist, function(index, value) {
+                // $()
+                $($.parseHTML('<div class="artist-grid-tile col-xs-6 col-sm-4 col-md-3 col-lg-2">' + value.name + '</div>'))
+                    .appendTo("#artist-grid-container .row");
+            });
+        })
+        .fail(function() {
+            alert('fail');
+        });
 });
+
+//
