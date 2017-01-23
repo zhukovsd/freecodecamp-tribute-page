fa32191976a532a170566c4636c7397703695a63
zhukovsd <zhukovsd@gmail.com>
2017-01-23 17:03:40 +0300

    Travis integration

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp tribute.html out/tribute.html
+
+mkdir -p out/less
+cp less/style.less out/less/style.less
+
+mkdir -p out/js
+cp js/app.js out/js/app.js
+cp js/bootstrap-toolkit.js out/js/bootstrap-toolkit.js
